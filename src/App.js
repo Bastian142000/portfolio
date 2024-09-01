11208adf3cd19f967a911b86227438d8515ccf30
Bastian142000 <bastian.prado.leiva@gmail.com>
2024-08-31 22:12:12 -0400

    Cards for the skill list added and minor color changes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,47 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { IoLogoReact } from "react-icons/io5";
+import Card from "react-bootstrap/Card";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { BsFiletypeSql } from "react-icons/bs";
+
+const skills = [
+  {
+    id: 1,
+    name: "Javascript",
+    level: "Intermediate",
+    description:
+      "Proficient in using JavaScript applied in frameworks like React, \
+       with a solid understanding of its syntax, data types, and control structures. \
+       Experienced in working with popular libraries and frameworks, and skilled in using \
+       JavaScript to solve real-world problems.",
+    icon: <IoLogoJavascript size={32} />,
+  },
+  {
+    id: 2,
+    name: "React",
+    level: "Intermediate",
+    description:
+      "Experienced in building reusable UI components with React. Skilled in managing state and props,\
+      and familiar with popular libraries like Redux and React Router. Able to build scalable \
+       and maintainable applications with React.",
+    icon: <FaReact size={32} />,
+  },
+  {
+    id: 3,
+    name: "SQL",
+    level: "Intermediate",
+    description:
+      "Proficient in writing efficient SQL queries to retrieve and manipulate data in \
+       relational databases. Experienced in designing and implementing database schemas, and \
+       skilled in using SQL to solve data analysis problems. Familiar with popular \
+       database management systems like Oracle SQL and SQLServer.",
+    icon: <BsFiletypeSql size={32} />,
+  },
+];
 
 export default function App() {
   return (
@@ -17,6 +55,7 @@ export default function App() {
       </header>
       <div className="app-body">
         <AboutMe />
+        <SkillList />
       </div>
       <footer className="app-footer"></footer>
     </div>
@@ -27,7 +66,7 @@ function NavbarPortfolio() {
   return (
     <Container>
       {/* Inicio del navbar, la clase Navbar es el contenedor principal y tiene todos los estilos necesarios */}
-      <Navbar id="navbar" className="m-2 mt-4" expand="sm">
+      <Navbar id="navbar" expand="sm">
         {/* Marca o Logo que se puede mostrar en el navbar */}
         <div className="d-sm mx-auto">
           <Navbar.Brand href="#">
@@ -88,8 +127,8 @@ function AboutMe() {
             hesitate to message me if you're interested!
           </span>
           <span>
-            <Button variant="dark">Projects</Button>
-            <Button variant="dark">Contact</Button>
+            <Button variant="success">Projects</Button>
+            <Button variant="success">Contact</Button>
           </span>
           <span className="aboutme-icons">
             <FaGithub size={32} className="git-icon" />
@@ -98,5 +137,42 @@ function AboutMe() {
         </Col>
       </Row>
     </Container>
+  );
+}
+
+function SkillList() {
+  return (
+    <Container className="mb-5">
+      <Row>
+        <Col sm={12} className="d-flex justify-content-center">
+          <Col sm={2} className="text-center" id="col-skills-titles">
+            <h2>These are my</h2>
+            <h3>Skills</h3>
+          </Col>
+        </Col>
+      </Row>
+      <Row>
+        {skills.map((skill) => (
+          <Col md={4} key={skill.id}>
+            <Skill skill={skill} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+function Skill({ skill }) {
+  return (
+    <Card className="mb-3" id="card">
+      <Card.Header id="card-header">
+        <span>{skill.icon} </span>
+        <span id="skill-name">{skill.name}</span>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>{skill.level}</Card.Title>
+        <Card.Text>{skill.description}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
